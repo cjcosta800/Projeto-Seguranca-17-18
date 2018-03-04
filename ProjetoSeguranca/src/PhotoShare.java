681e2e816a9d34321d5190e41598d3e6079f5936
Tiago Santos <tiago.lajes@gmail.com>
2018-03-04 18:27:31 +0000

    check args

--- ProjetoSeguranca/src/PhotoShare.java
+++ ProjetoSeguranca/src/PhotoShare.java
@@ -44,9 +44,6 @@ public class PhotoShare {
 			outStream.close();
 			inStream.close();
 			socket.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
