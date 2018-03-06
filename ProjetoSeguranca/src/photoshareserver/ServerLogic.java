44fcd417eba4b28f11dd0c04d5c02ecb29e767a1
Frederico Apolónia <fredericojcapolo@gmail.com>
2018-03-06 17:10:57 +0000

    Indentation

--- ProjetoSeguranca/src/photoshareserver/ServerLogic.java
+++ ProjetoSeguranca/src/photoshareserver/ServerLogic.java
@@ -5,20 +5,23 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.Socket;
 import java.util.HashMap;
 
 public class ServerLogic {
 
 	private String passwordsPath;
 	private HashMap<String, String> userPwd;
-	
-	
+	private String user;
+
+
 	public ServerLogic(String passwordsPath) {
-		
+
 		this.passwordsPath = passwordsPath;
-		
+
+
 	}
-	
+
 	/**
 	 * Authenticates user. If user doesn't exist, creates a new one with 
 	 * the given user and password.
@@ -30,20 +33,28 @@ public class ServerLogic {
 	 * @throws IOException
 	 */
 	public boolean getAuthenticated(String user, String password) throws IOException {
-		
+
 		this.userPwd = loadPasswords();
-		
+
 		if(userPwd.containsKey(user)) {
-			return userPwd.get(user).equals(password);
+
+			if(userPwd.get(user).equals(password)) {
+				this.user = user;
+				return true;
+			}
+			else
+				return false;
 		}
 		else {
 			registerUser(user, password);
-			
+
+			this.user = user;
+
 			return true;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Adds a new user to the passwords file
 	 * @param user
@@ -51,45 +62,45 @@ public class ServerLogic {
 	 * @throws IOException
 	 */
 	private void registerUser(String user, String password) throws IOException {
-		
+
 		BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.passwordsPath, true));
-		
+
 		fileWriter.write(user + ":" + password);
-		
+
 		fileWriter.close();
-		
+
 	}
-	
+
 	/**
 	 * Loads users and passwords from the passwords file (provided by passwordsPath)
 	 * @return HashMap<User, Password> containing all users and corresponding password
 	 * @throws IOException
 	 */
 	private HashMap<String, String> loadPasswords() throws IOException {
-		
+
 		BufferedReader filereader = new BufferedReader(new FileReader(this.passwordsPath));
 
-        String line = filereader.readLine();
+		String line = filereader.readLine();
 
-        // HashMap <User, Password>
-        HashMap<String, String> userpwd = new HashMap<>();
-        String tokenised[] = null;
-        // user;password
-        while (line != null) {
+		// HashMap <User, Password>
+		HashMap<String, String> userpwd = new HashMap<>();
+		String tokenised[] = null;
+		// user;password
+		while (line != null) {
 
-            tokenised = line.split(":");
+			tokenised = line.split(":");
 
-            userpwd.put(tokenised[0], tokenised[1]);
+			userpwd.put(tokenised[0], tokenised[1]);
 
-            line = filereader.readLine();
+			line = filereader.readLine();
 
-        }
+		}
 
-        System.out.println("Import complete.");
-        
-        filereader.close();
+		System.out.println("Import complete.");
 
-        return userpwd;
+		filereader.close();
+
+		return userpwd;
 	}
-	
+
 }
