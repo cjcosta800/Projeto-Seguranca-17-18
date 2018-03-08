f4727c7eab91e6a2549d4b1e7cfd0d68e6554e0b
Tiago Santos <tiago.lajes@gmail.com>
2018-03-08 17:45:36 +0000

    ClientLogic has been fixed, not tested

--- ProjetoSeguranca/src/photoshareclient/ClientLogic.java
+++ ProjetoSeguranca/src/photoshareclient/ClientLogic.java
@@ -1,107 +1,131 @@
 package photoshareclient;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientLogic {
 
-    private Socket socket;
-    private String currUser;
+	private Socket socket;
+	private String currUser;
 
-    public ClientLogic(String currUser, Socket socket) {
-        this.currUser = currUser;
-        this.socket = socket;
-    }
+	public ClientLogic(String currUser, Socket socket) {
+		this.currUser = currUser;
+		this.socket = socket;
+	}
 
-    public void sendPhotos(String photos) {
+	public void sendPhotos(String photos) {
 
-    }
+	}
 
-    public void listPhotos (String user) {
+	public void listPhotos (String user) {
 
 
-    }
+	}
 
-    public void likeDislikeCounter (String userid, String photoName) {
+	public void likeDislikeCounter (String userid, String photoName) {
 
-    }
+	}
 
-    public void backupAllPhotos(String userid) {
+	public void backupAllPhotos(String userid) {
 
-    }
+	}
 
-    public void commentPhoto(String comment,String userid,String photoName) {
+	public void commentPhoto(String comment,String userid,String photoName) {
 
-    }
+	}
 
-    public void likePhoto(String userid, String photoName) {
+	public void likePhoto(String userid, String photoName) {
 
-    }
+	}
 
-    public void dislikePhoto(String userid, String photoName) {
-        currUser
-    }
+	public void dislikePhoto(String userid, String photoName) {
 
-    public void followLocalUser(String users) {
+	}
 
-        String[] usersList = users.split(",");
-        String followersPath = "./src/photoshareserver/Photos/" + user + "/followers.txt";
-        File followers = new File(followersPath);
-        if (!followers.isFile) {
-            System.out.println("Ficheiro não existe!");
-            return;
-        }
+	public void followLocalUser(String users) {
 
-        BufferedReader buffReader = new BufferedReader(new FileReader(followers);
-        PrintWriter pw = new PrintWriter(new FileWriter(followers,true));
+		String[] usersList = users.split(",");
+		String followersPath = "./src/photoshareserver/Photos/" + currUser + "/followers.txt";
 
-        while((String user = buffReader.readLine()) != null) {
-            for (int i = 0; i < usersList.length; i++) {
-                if (!user.trim().equals(usersList[i])) {
-                    pw.println(user);
-                    pw.flush();
-                }
-            }
-        }
-        pw.close();
-        buffReader.close();
+		try {
+			File followers = new File(followersPath);
+			if (!followers.isFile()) {
+				System.out.println("Ficheiro não existe!");
+				return;
+			}
 
-        }
+			BufferedReader buffReader = new BufferedReader(new FileReader(followers));
+			PrintWriter pw = new PrintWriter(new FileWriter(followers));
+			String user;
 
-    public void unfollowLocalUser(String users) { //FALTAM OS ERROS
-        String[] usersList = users.split(",");
-        String followersPath = "./src/photoshareserver/Photos/" + user + "/followers.txt";
-        File followers = new File(followersPath);
-        if (!followers.isFile) {
-            System.out.println("Ficheiro não existe!");
-            return;
-        }
+			while((user = buffReader.readLine()) != null) {
+				for (int i = 0; i < usersList.length; i++) {
+					if (!user.trim().equals(usersList[i])) {
+						pw.println(user);
+						pw.flush();
+					}
+				}
+			}
+			pw.close();
+			buffReader.close();
 
-        File aux = new File (followersPath + ".tmp");
-        BufferedReader buffReader = new BufferedReader(new FileReader(followers);
-        PrintWriter pw = new PrintWriter(new FileWriter(aux));
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-        while((String user = buffReader.readLine()) != null) {
-            for (int i = 0; i < usersList.length; i++) {
-                if (!user.trim().equals(usersList[i])) {
-                    pw.println(user);
-                    pw.flush();
-                }
-            }
-            }
-            pw.close();
-            buffReader.close();
+	public void unfollowLocalUser(String users) { //FALTAM OS ERROS
+		String[] usersList = users.split(",");
+		String followersPath = "./src/photoshareserver/Photos/" + currUser + "/followers.txt";
 
-            if (!followers.delete()) {
-                System.out.println("Ficheiro não foi removido!");
-                return;
-            }
+		try {
+			File followers = new File(followersPath);
+			if (!followers.isFile()) {
+				System.out.println("Ficheiro não existe!");
+				return;
+			}
 
-            if (!aux.renameTo(followers)) {
-                System.out.println("Nome do ficheiro não foi alterado!");
-            }
-        }
+			File aux = new File (followersPath + ".tmp");
+			BufferedReader buffReader = new BufferedReader(new FileReader(followers));
+			PrintWriter pw = new PrintWriter(new FileWriter(aux));
+			String user;
 
-        //LER FICHEIRO TODO, PROCURAR USERS A REMOVER, NAO ABRIR COM PARAMETRO A TRUE, ESCREVER DE NOVO O FICHEIRO SEM INCLUIR O USER QUE ACABAMOS DE REMOVER.
-        //REESCREVER O FICHEIRO SEM O USER QUE SE VAI EXCLUIR.
-    }
+			while((user = buffReader.readLine()) != null) {
+				for (int i = 0; i < usersList.length; i++) {
+					if (!user.trim().equals(usersList[i])) {
+						pw.println(user);
+						pw.flush();
+					}
+				}
+			}
+			pw.close();
+			buffReader.close();
+
+			if (!followers.delete()) {
+				System.out.println("Ficheiro não foi removido!");
+				return;
+			}
+
+			if (!aux.renameTo(followers)) {
+				System.out.println("Nome do ficheiro não foi alterado!");
+			}
+
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
+
+//LER FICHEIRO TODO, PROCURAR USERS A REMOVER, NAO ABRIR COM PARAMETRO A TRUE, ESCREVER DE NOVO O FICHEIRO SEM INCLUIR O USER QUE ACABAMOS DE REMOVER.
+//REESCREVER O FICHEIRO SEM O USER QUE SE VAI EXCLUIR.
+
