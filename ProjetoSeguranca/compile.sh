ef07fc285d518ee22da907f6275a72a09ead3029
Frederico Apolónia <fc47892@alunos.fc.ul.pt>
2018-03-17 15:56:49 +0000

    Minor compile file changes

--- ProjetoSeguranca/compile.sh
+++ ProjetoSeguranca/compile.sh
@@ -9,36 +9,35 @@ CLIENT_IN_PATH='PhotoShareClient/src'
 ########################################################
 ####      CHECK IF SERVER AND CLIENT OUT EXISTS     ####
 ########################################################
-if [[ ! -d SERVER_OUT_PATH ]]; then
+if [[ ! -d $SERVER_OUT_PATH ]]; then
   mkdir $SERVER_OUT_PATH
 fi
 
-if [[ ! -d CLIENT_OUT_PATH ]]; then
+if [[ ! -d $CLIENT_OUT_PATH ]]; then
   mkdir $CLIENT_OUT_PATH
 fi
 
 ########################################################
 ####      DELETE SERVER AND CLIENT OUT              ####
 ########################################################
-if [[ "$(ls SERVER_OUT_PATH -A)" ]]; then
+if [[ "$(ls $SERVER_OUT_PATH -A)" ]]; then
   echo Removing old server files
   rm $SERVER_OUT_PATH/*
-fi
 else
   echo Server out directory is already empty
+fi
 
-if [[ "$(ls CLIENT_OUT_PATH -A)" ]]; then
+if [[ "$(ls $CLIENT_OUT_PATH -A)" ]]; then
   echo Removing old server files
   rm $CLIENT_OUT_PATH/*
-fi
 else
   echo Client out directory is already empty
+fi
 
 ########################################################
 ####      COMPILE SERVER AND CLIENT                 ####
 ########################################################
 echo Compiling Server
 javac -cp $SERVER_OUT_PATH/ -d $SERVER_OUT_PATH/ $SERVER_IN_PATH/*.java
-# javac -cp $SERVER_OUT_PATH/ -d $SERVER_OUT_PATH/ ./PhotoShareServer/src/PhotoShareServer.java
 echo Compiling Client
 javac -cp $CLIENT_OUT_PATH/ -d $CLIENT_OUT_PATH/ $CLIENT_IN_PATH/*.java
