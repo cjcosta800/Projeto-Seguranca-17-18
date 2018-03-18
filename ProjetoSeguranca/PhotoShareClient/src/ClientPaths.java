368c3586af11a62cf92b1e5eae69c44c77ccccbe
Frederico Apolónia <fc47892@alunos.fc.ul.pt>
2018-03-18 13:21:17 +0000

    fixed client  path

--- ProjetoSeguranca/PhotoShareClient/src/ClientPaths.java
+++ ProjetoSeguranca/PhotoShareClient/src/ClientPaths.java
@@ -2,5 +2,5 @@ import java.io.File;
 
 public class ClientPaths {
     public static final String FILE_SEPARATOR = File.separator;
-    public static final String CLIENT_PATH = ".." + FILE_SEPARATOR;
+    public static final String CLIENT_PATH = "PhotoShareClient" + FILE_SEPARATOR;
 }
